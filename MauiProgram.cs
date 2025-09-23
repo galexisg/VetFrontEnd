c1a78183d23a6ad83c4a868ab8466261de881068
erickwc <erickcaceres1717@gmail.com>
2025-09-23 10:18:00 -0600

    Agregar archivos de proyecto.

---
+++ MauiProgram.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Veterinaria.MAUIApp
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                });
+
+            builder.Services.AddMauiBlazorWebView();
+
+#if DEBUG
+    		builder.Services.AddBlazorWebViewDeveloperTools();
+    		builder.Logging.AddDebug();
+#endif
+
+            builder.Services.AddScoped(http => new HttpClient
+            {
+                BaseAddress = new Uri("https://localhost:7174/")
+            });
+
+
+            /*Añadir servicios*/
+            
+
+            return builder.Build();
+        }
+    }
+}
---
+++ Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Veterinaria.MAUIApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
