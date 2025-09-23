dd1584870eb06166c538d670e617a9fdebf91069
erickwc <erickcaceres1717@gmail.com>
2025-09-23 12:49:02 -0600

    Agregar Veterinaria.MAUIApp como carpeta normal

---
+++ Veterinaria.MAUIApp/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Veterinaria.MAUIApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
