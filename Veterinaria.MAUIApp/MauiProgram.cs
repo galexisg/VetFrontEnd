54dd2198f41d26fb6242c614af0980dc29dcaef8
leydi <leydi@LEYDIPEREZ5>
2025-09-29 11:38:39 -0600

    Vistas de vacunas

--- Veterinaria.MAUIApp/MauiProgram.cs
+++ Veterinaria.MAUIApp/MauiProgram.cs
@@ -80,6 +80,7 @@ namespace Veterinaria.MAUIApp
             builder.Services.AddScoped<AuthService>();
             builder.Services.AddScoped<DiagnosticoService>();
             builder.Services.AddScoped<TratamientosAplicadosService>();
+            builder.Services.AddScoped<VacunaService>();
 
             return builder.Build();
         }
--- Veterinaria.MAUIApp/Models/Vacuna.cs
+++ Veterinaria.MAUIApp/Models/Vacuna.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace Veterinaria.MAUIApp.Models
 {
-    internal class Vacuna
+    public class Vacuna
     {
+        [JsonPropertyName("vacunaId")]
+        public int Id { get; set; }
+
+        [JsonPropertyName("nombre")]
+        public string Nombre { get; set; } = string.Empty;
+
+        [JsonPropertyName("estado")]
+        public bool Estado { get; set; }
+    }
+
+    // Lo que envías al backend
+    public class VacunaReq
+    {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [StringLength(80, ErrorMessage = "Máximo 80 caracteres")]
+        [JsonPropertyName("nombre")]
+        public string Nombre { get; set; } = string.Empty;
     }
 }
+
