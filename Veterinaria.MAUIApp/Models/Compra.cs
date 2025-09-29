044919936d449ad400c34b2871d7f954dbe2e254
Cristian Torres <miguel29torres12@gmail.com>
2025-09-29 09:24:53 -0600

    cambios

--- Veterinaria.MAUIApp/Models/Compra.cs
+++ Veterinaria.MAUIApp/Models/Compra.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace Veterinaria.MAUIApp.Models
 {
+<<<<<<< Updated upstream
     public class Compra
     {
         // Identificador único de la compra (clave primaria)
@@ -21,6 +22,32 @@ namespace Veterinaria.MAUIApp.Models
         public int UsuarioId { get; set; }
 
 
+=======
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    namespace Veterinaria.MAUIApp.Models
+    {
+        public class Compra
+        {
+            // Identificador único de la compra (clave primaria)
+            public int CompraId { get; set; }
+
+            // Identificador del proveedor asociado a la compra
+            public int ProveedorId { get; set; }
+
+            // Fecha y hora en que se realizó la compra
+            public DateTime FechaCompra { get; set; }
+
+            // Identificador del usuario que registró o autorizó la compra
+            public int UsuarioId { get; set; }
+
+
+        }
+>>>>>>> Stashed changes
     }
 }
 
