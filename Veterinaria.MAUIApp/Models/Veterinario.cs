6fc362f7ef7d7afd91c3191f6eb507af7769582e
Hugo Matew Gonzalez Contreras <contrerashugo680@gmail.com>
2025-09-30 02:10:59 -0600

    vista agregar funcional DetalleHorarioVeterinario

--- Veterinaria.MAUIApp/Models/Veterinario.cs
+++ Veterinaria.MAUIApp/Models/Veterinario.cs
@@ -1,12 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Veterinaria.MAUIApp.Models
 {
-    internal class Veterinario
+
+
+    public class VeterinarioSalidaRes
     {
+        public int Id { get; set; }
+        public string NumeroLicencia { get; set; } = string.Empty;
+        public string Estado { get; set; } = "Activo";
+        public string Especialidad { get; set; } = string.Empty;
+        public string Servicio { get; set; } = string.Empty;
+        public Usuario Usuario { get; set; } = new();
+    }
+    public class VeterinarioModificarReq
+    {
+        public int Id { get; set; }
+        public string NumeroLicencia { get; set; } = string.Empty;
+        public int UsuarioId { get; set; }
+        public int EspecialidadId { get; set; }
+        public long ServicioId { get; set; }
+        public string Estado { get; set; } = "Activo";
+    }
+    public class VeterinarioGuardarReq
+    {
+        public string NumeroLicencia { get; set; } = string.Empty;
+
+        public int? UsuarioId { get; set; }
+
+        public int? EspecialidadId { get; set; }
+
+
+        public int? ServicioId { get; set; }
+
+
+
+        public class EspecialidadRes
+        {
+            public int Id { get; set; }
+            public string Nombre { get; set; } = string.Empty;
+        }
+
+        public class ServicioRes
+        {
+            public long Id { get; set; }
+            public string Nombre { get; set; } = string.Empty;
+        }
+
     }
 }
